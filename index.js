c2701675aecadcb62674cf0b56f030f5ac56bdc2
sndlmncwango <sndlmncwango@gmail.com>
2023-01-27 20:50:36 +0200

    project complete

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const container = document.querySelector("#container")
-
-for(let i=0;i<2000;i++){
-    const divs = document.createElement("div");
-    divs.setAttribute("class","box")
-    container.appendChild(divs);
-    divs.addEventListener("mousemove",(e)=>{
-        divs.setAttribute("class","hover")
+const container = document.querySelector("#container");
+const gridLayout = document.querySelector("#gridLayout");
+gridLayout.addEventListener("click",(e)=>{
+    userColumn = parseInt(prompt("enter number of columns"))
+    if(userColumn <= 100 && userColumn >= 0){
+    columnCount(userColumn)
+    }else{alert("enter vaule less 1-100")}
+})
+function columnCount(num){
+    let numberColumn = userColumn * userColumn;
+    let w_h = (560 / userColumn)-2
+    console.log(numberColumn,w_h)
+    for(let i=0;i<numberColumn;i++){
+        const divs = document.createElement("div");
+        divs.setAttribute("class","box")
+        container.appendChild(divs);
+        divs.style.width = `${w_h}px`;
+        divs.style.height = `${w_h}px`;
+        divs.addEventListener("mousemove",(e)=>{
+            divs.setAttribute("class","hover")
     })
+}
 }
