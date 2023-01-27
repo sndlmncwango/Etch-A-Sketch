7ae307a2b32d2fb859127d71a272e3fdb0afb454
sndlmncwango <sndlmncwango@gmail.com>
2023-01-27 21:00:57 +0200

    added random color picker"
    q

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const container = document.querySelector("#container");
 const gridLayout = document.querySelector("#gridLayout");
+
 gridLayout.addEventListener("click",(e)=>{
     userColumn = parseInt(prompt("enter number of columns"))
     if(userColumn <= 100 && userColumn >= 0){
     columnCount(userColumn)
-    }else{alert("enter vaule less 1-100")}
+    }else{alert("enter number 1-100")}
 })
 function columnCount(num){
     let numberColumn = userColumn * userColumn;
@@ -16,8 +17,13 @@ function columnCount(num){
         container.appendChild(divs);
         divs.style.width = `${w_h}px`;
         divs.style.height = `${w_h}px`;
+        
         divs.addEventListener("mousemove",(e)=>{
             divs.setAttribute("class","hover")
+            let rand1 = Math.floor(Math.random()*255);
+            let rand2 = Math.floor(Math.random()*255);
+            let rand3 = Math.floor(Math.random()*255);
+            divs.style.backgroundColor = `rgb(${rand1}, ${rand2}, ${rand3})`
     })
 }
 }
